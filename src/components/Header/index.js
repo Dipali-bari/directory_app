285438f10f6aa859e8b09652196665ed2454c7b8
LalanBhuwan <bhuwanlalan475@gmail.com>
2023-08-16 22:25:24 +0530

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+
+import "./header.css";
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Bhuwan's Directory App</h1>
+    </div>
+  );
+};
+
+export default Header;
